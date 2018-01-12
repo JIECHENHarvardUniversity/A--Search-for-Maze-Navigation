2aa70e175078a73f14ad06af370c4d9f26e51b5f
JIECHENHarvardUniversity <34819601+JIECHENHarvardUniversity@users.noreply.github.com>
2018-01-13 04:49:11 +0800

    Update Astar.h

--- Astar.h
+++ Astar.h
@@ -1,19 +1,16 @@
-#pragma once  
-/* 
-//A*算法对象类 
-*/  
+//define the object containing the A* algorithm
 #include <vector>  
 #include <list>  
   
-const int kCost1=10; //直移一格消耗  
-const int kCost2=14; //斜移一格消耗  
+const int kCost1=10; //define the cost of agent when moving one step axially  
+const int kCost2=14; //define the cost of agent when moving one step diagonally  
   
 struct Point  
 {  
-    int x,y; //点坐标，这里为了方便按照C++的数组来计算，x代表横排，y代表竖列  
+    int x,y; //coordinates of the agent 
     int F,G,H; //F=G+H  
-    Point *parent; //parent的坐标，这里没有用指针，从而简化代码  
-    Point(int _x,int _y):x(_x),y(_y),F(0),G(0),H(0),parent(NULL)  //变量初始化  
+    Point *parent; //coordinates of the parent node  
+    Point(int _x,int _y):x(_x),y(_y),F(0),G(0),H(0),parent(NULL)  //initialize the variables  
     {  
     }  
 };  
@@ -28,15 +25,17 @@ public:
 private:  
     Point *findPath(Point &startPoint,Point &endPoint,bool isIgnoreCorner);  
     std::vector<Point *> getSurroundPoints(const Point *point,bool isIgnoreCorner) const;  
-    bool isCanreach(const Point *point,const Point *target,bool isIgnoreCorner) const; //判断某点是否可以用于下一步判断  
-    Point *isInList(const std::list<Point *> &list,const Point *point) const; //判断开启/关闭列表中是否包含某点  
-    Point *getLeastFpoint(); //从开启列表中返回F值最小的节点  
-    //计算FGH值  
+    bool isCanreach(const Point *point,const Point *target,bool isIgnoreCorner) const; 
+    //determine whether a node can be used in the next step  
+    Point *isInList(const std::list<Point *> &list,const Point *point) const; 
+    //determine whether a node has been included in the openList or the closeList  
+    Point *getLeastFpoint(); //return node in the openList with the least F value  
+    //calculate the values of F, G, and H  
     int calcG(Point *temp_start,Point *point);  
     int calcH(Point *point,Point *end);  
     int calcF(Point *point);  
 private:  
     std::vector<std::vector<int>> maze;  
-    std::list<Point *> openList;  //开启列表  
-    std::list<Point *> closeList; //关闭列表  
+    std::list<Point *> openList;  //define the openList  
+    std::list<Point *> closeList; //define the closeList 
 };  
