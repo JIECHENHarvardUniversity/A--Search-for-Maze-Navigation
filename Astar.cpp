9424fa97fbe7caebf7c3a66eafde8ddea2c705ee
JIECHENHarvardUniversity <34819601+JIECHENHarvardUniversity@users.noreply.github.com>
2018-01-13 04:40:35 +0800

    Update Astar.cpp

--- Astar.cpp
+++ Astar.cpp
@@ -9,13 +9,13 @@ void Astar::InitAstar(std::vector<std::vector<int>> &_maze)
 int Astar::calcG(Point *temp_start,Point *point)  
 {  
     int extraG=(abs(point->x-temp_start->x)+abs(point->y-temp_start->y))==1?kCost1:kCost2;  
-    int parentG=point->parent==NULL?0:point->parent->G; //如果是初始节点，则其父节点是空  
+    int parentG=point->parent==NULL?0:point->parent->G; //if it is the initial node, then its parent mode is void
     return parentG+extraG;  
 }  
   
 int Astar::calcH(Point *point,Point *end)  
 {  
-    //用简单的欧几里得距离计算H，这个H的计算是关键，还有很多算法，没深入研究^_^  
+    //H is calculate d with Euclid distance
     return sqrt((double)(end->x-point->x)*(double)(end->x-point->x)+(double)(end->y-point->y)*(double)(end->y-point->y))*kCost1;  
 }  
   
@@ -39,17 +39,19 @@ Point *Astar::getLeastFpoint()
   
 Point *Astar::findPath(Point &startPoint,Point &endPoint,bool isIgnoreCorner)  
 {  
-    openList.push_back(new Point(startPoint.x,startPoint.y)); //置入起点,拷贝开辟一个节点，内外隔离  
+    openList.push_back(new Point(startPoint.x,startPoint.y));
+    //put the initial node into the openList, create a new node by copy, and separate the two nodes
     while(!openList.empty())  
     {  
-        auto curPoint=getLeastFpoint(); //找到F值最小的点  
-        openList.remove(curPoint); //从开启列表中删除  
-        closeList.push_back(curPoint); //放到关闭列表  
-        //1,找到当前周围八个格中可以通过的格子  
+        auto curPoint=getLeastFpoint(); //find the curPoint with the least F value  
+        openList.remove(curPoint); //delete the curPoint from the openList  
+        closeList.push_back(curPoint); //store the curPoint in the closeList  
+        //1. find the eight accessible nodes in the neighborhood of the current node  
         auto surroundPoints=getSurroundPoints(curPoint,isIgnoreCorner);  
         for(auto &target:surroundPoints)  
         {  
-            //2,对某一个格子，如果它不在开启列表中，加入到开启列表，设置当前格为其父节点，计算F G H  
+            //2. for any node, put it into the openList is it is not included in the openList
+            //set the current node as its parent node, and calculate the corresponding F, G, and H
             if(!isInList(openList,target))  
             {  
                 target->parent=curPoint;  
@@ -60,7 +62,9 @@ Point *Astar::findPath(Point &startPoint,Point &endPoint,bool isIgnoreCorner)
   
                 openList.push_back(target);  
             }  
-            //3，对某一个格子，它在开启列表中，计算G值, 如果比原来的大, 就什么都不做, 否则设置它的父节点为当前点,并更新G和F  
+            //3. for any node, if it has already been included in the openList then calculate the G value
+            //if the G value is larger than the previous one, leave it unchnaged\
+            //otherwise set its parent node as the current node and update the G and F
             else  
             {  
                 int tempG=calcG(curPoint,target);  
@@ -74,7 +78,9 @@ Point *Astar::findPath(Point &startPoint,Point &endPoint,bool isIgnoreCorner)
             }  
             Point *resPoint=isInList(openList,&endPoint);  
             if(resPoint)  
-                return resPoint; //返回列表里的节点指针，不要用原来传入的endpoint指针，因为发生了深拷贝  
+                return resPoint;
+                //return pointer of node in the list
+                //do not use the previously passed endpoint pointer because it has been deep copied
         }  
     }  
   
@@ -85,7 +91,7 @@ std::list<Point *> Astar::GetPath(Point &startPoint,Point &endPoint,bool isIgnor
 {  
     Point *result=findPath(startPoint,endPoint,isIgnoreCorner);  
     std::list<Point *> path;  
-    //返回路径，如果没找到路径，返回空链表  
+    //return the path, return a void list if no path has been found  
     while(result)  
     {  
         path.push_front(result);  
@@ -96,7 +102,7 @@ std::list<Point *> Astar::GetPath(Point &startPoint,Point &endPoint,bool isIgnor
   
 Point *Astar::isInList(const std::list<Point *> &list,const Point *point) const  
 {  
-    //判断某个节点是否在列表中，这里不能比较指针，因为每次加入列表是新开辟的节点，只能比较坐标  
+    //determine whether a given node has already been included in tyhe openList
     for(auto p:list)  
         if(p->x==point->x&&p->y==point->y)  
             return p;  
@@ -109,15 +115,20 @@ bool Astar::isCanreach(const Point *point,const Point *target,bool isIgnoreCorne
         ||target->y<0&&target->y>maze[0].size()-1  
         ||maze[target->x][target->y]==1  
         ||target->x==point->x&&target->y==point->y  
-        ||isInList(closeList,target)) //如果点与当前节点重合、超出地图、是障碍物、或者在关闭列表中，返回false  
+        ||isInList(closeList,target))
+        //return false 
+        //when the point coincides with current node
+        //when the point is out of the map
+        //when the point is an belongs to any obstacle
+        //when the point is in the closeList
         return false;  
     else  
     {  
-        if(abs(point->x-target->x)+abs(point->y-target->y)==1) //非斜角可以  
+        if(abs(point->x-target->x)+abs(point->y-target->y)==1) //when the agent is not moving diagonally, it is OK   
             return true;  
         else  
         {  
-            //斜对角要判断是否绊住  
+            //determine whether the agent is stucked once it is moving diagonally  
             if(maze[point->x][target->y]==0&&maze[target->x][point->y]==0)  
                 return true;  
             else  
